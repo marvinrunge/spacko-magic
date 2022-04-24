ed3af1989f39be387d0b08d0d57e5e84378a7a24
marvinrunge <marvinrunge@web.de>
2022-04-24 13:50:19 +0200

    asd

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(spacko-magic.de www.spacko-magic.de)
+domains=("spacko-magic.de" "www.spacko-magic.de")
 rsa_key_size=4096
 data_path="./data/certbot"
 email="marvinrunge@web.de" # Adding a valid address is strongly recommended
