afff50cb4c0bd2e6c9f5e193feb5ef79f440fa46
Marvin Runge <marvinrunge@web.de>
2022-07-15 11:29:20 +0200

    remove backround animation

--- apps/spacko-magic/src/app/app.component.ts
+++ apps/spacko-magic/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.authService.checkSession();
 
-    this.setBackgroundPosition();
+    // this.setBackgroundPosition();
   }
 
   async setBackgroundPosition() {
